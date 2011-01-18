210941f9a31af534e59acbc20129dd86189dd368
Pouya Rezaeifakhr <pouya@lavabit.com>
2011-01-18 10:56:22 -0800

    wrote tests for should and should_not error messages

--- lib/rspec_w3c_matchers/matchers.rb
+++ lib/rspec_w3c_matchers/matchers.rb
@@ -1,21 +1,24 @@
 module RspecW3cMatchers
   
   module InstanceMethods  
-    def contain_w3c_errors_in(page)
-      W3cMatchers::Errors.new(page,:==,0,:errors)
+    def have_w3c_errors_in(page)
+      W3cMatchers::Errors.new(page,:>,0,:errors)
     end
 
-    def contain_w3c_warnings_in(page)
-      W3cMatchers::Warnings.new(page,:==,0,:warnings)
+    def have_w3c_warnings_in(page)
+      W3cMatchers::Warnings.new(page,:>,0,:warnings)
     end
 
-    def contain_specified_number_of_errors_in(page,number)
+    def have_specified_number_of_errors_in(page,number)
       W3cMatchers::Errors.new(page,:==,number,:errors)
     end
     
-    def contain_specified_number_of_warnings_in(page,number)
+    def have_specified_number_of_warnings_in(page,number)
       W3cMatchers::Warnings.new(page,:==,number,:warnings)
     end
+    
+    
+    
   end
 
 end
--- spec/shared_examples/notice_matcher.rb
+++ spec/shared_examples/notice_matcher.rb
@@ -51,9 +51,10 @@ shared_examples_for "w3c matcher for :>= operator" do
   end   
 end
 
-shared_examples_for "w3c matcher with detailed notice reporting" do
+shared_examples_for "w3c matcher with detailed notice reporting" do      
+  let(:regexp_should_header){Regexp.new("Expected (?<expected>\\d+) W3C validation #{type.to_s}? but received (?<actual>\\d+) #{type.to_s}?")}
+  let(:regexp_should_not_header){Regexp.new("Did not expect (?<expected>\\d+) W3C validation #{type.to_s}? but received (?<actual>\\d+) #{type.to_s}?")}
   
-        
   context ":failure_message_for_should" do
     before(:each) do
       @matcher = klass.new(dirty_page,:==,0,type)
@@ -62,21 +63,11 @@ shared_examples_for "w3c matcher with detailed notice reporting" do
     end
     
     let(:msg){@matcher.failure_message_for_should.split("\n")}
-    
-    it "returns proper header" do    
-      num_matches(msg, /Expected \d+ W3C validation (errors|warnings) but received \d+ errors/)      
-    end
-    it "returns proper validator string" do
-      num_matches(msg,/Checked by: \S+/).should be 1
-    end    
-    it "returns proper character set" do
-      num_matches(msg,/Character set: \S+/).should be 1
-    end
-    it "returns proper number of notices" do      
-      num_notices(msg)
-      num_matches(msg,/#{type.to_s.capitalize}s?: \S+/).should be 1
-    end
         
+    it "returns proper header" do          
+      num_matches(msg, regexp_should_header).should be 1     
+    end
+    it_behaves_like "w3c detailed notices"
   end
   
   context ":failure_message_for_should_not" do
@@ -89,9 +80,10 @@ shared_examples_for "w3c matcher with detailed notice reporting" do
     let(:matcher){@matcher}
     let(:msg){@matcher.failure_message_for_should_not.split("\n")}
     
-    it "returns proper header" do
-      num_matches(msg,/Did not expect \d+ W3C validation #{type}s? but received \d+ #{type}s?/)
+    it "returns proper header" do      
+      num_matches(msg,regexp_should_not_header).should be 1
     end
+    it_behaves_like "w3c detailed notices"
   end
   
   def num_matches(array,regexp)
@@ -106,6 +98,44 @@ shared_examples_for "w3c matcher with detailed notice reporting" do
       matches.nil? ? nil : matches[1].to_i
     end.keep_if do |value| !value.nil? end.first
   end
-
+  
+  def expected_notices(array)
+    abc = array.map do |string|
+      
+      if (matches = regexp_should_header.match(string)) || (matches = regexp_should_not_header.match(string))
+        matches['expected'].to_i        
+      else
+        nil
+      end      
+    end.keep_if do |value| !value.nil? end.first      
+  end
 end
+
+shared_examples_for "w3c detailed notices" do
+  it "returns proper validator string" do    
+    num_matches(msg,/^\s*Checked by: \S+/).should be 1
+  end    
+  it "returns proper character set" do
+    num_matches(msg,/^\s*Character set: \S+/).should be 1
+  end
+  it "returns proper number of notices" do          
+    num_matches(msg,/^\s*#{type.to_s.capitalize}s?: \S+/).should be 1
+  end
+  it "returns Type declaration for every notice" do
+    num_matches(msg,/^\s*Type: #{type}?/).should be num_notices(msg)
+  end
+  it "returns Location declaration for every notice" do
+    num_matches(msg,/^\s*Location: line \S+ column \S+/).should be num_notices(msg)
+  end
+  it "returns URI declaration for every notice" do
+    num_matches(msg,/^\s*URI: .*/).should be num_notices(msg)
+  end
+  it "returns Message declaration for every notice" do
+    num_matches(msg,/^\s*Message: .*/).should be num_notices(msg)
+  end
+  it "returns expected number of Source declarations" do    
+    num_matches(msg,/^\s*Source \(line \d*\): .*/).should be num_notices(msg)
+  end
+end
+
 
